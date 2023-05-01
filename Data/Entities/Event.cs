7f447c968653da6d16730a7984634d099a8b9d5a
Borislav Yurkev <borislavurkev@gmail.com>
2023-05-01 16:15:08 +0300

    Add entities, context and migrations. Make database

---
+++ Data/Entities/Event.cs
@@ -0,0 +1,30 @@
+﻿namespace Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using static Data.DataConstants.Event;
+
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(EventNameMaxLenght)]
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Required]
+        [MaxLength(EventDescriptionMaxLenght)]
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [Required]
+        public int DeviceId { get; set; }
+
+        [ForeignKey(nameof(DeviceId))]
+        public Device Device { get; set; }
+    }
+}
---
+++ Data/Migrations/20230501130502_InitialMigration.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appliances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsTurnOn = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appliances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    PowerUsage = table.Column<float>(type: "real", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", maxLength: 2097152, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplianceDevice",
+                columns: table => new
+                {
+                    AppliancesId = table.Column<int>(type: "int", nullable: false),
+                    DevicesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplianceDevice", x => new { x.AppliancesId, x.DevicesId });
+                    table.ForeignKey(
+                        name: "FK_ApplianceDevice_Appliances_AppliancesId",
+                        column: x => x.AppliancesId,
+                        principalTable: "Appliances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplianceDevice_Devices_DevicesId",
+                        column: x => x.DevicesId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    DeviceId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplianceDevice_DevicesId",
+                table: "ApplianceDevice",
+                column: "DevicesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_DeviceId",
+                table: "Events",
+                column: "DeviceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplianceDevice");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Appliances");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+        }
+    }
+}
